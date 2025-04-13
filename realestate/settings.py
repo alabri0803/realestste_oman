229a2abdfb399ced0b3339f5c6ca611891816992
alabri0803 <rashid92759346@gmail.com>
2025-04-13 16:32:17 +0000

    se

--- realestate/settings.py
+++ realestate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +116,7 @@ USE_I18N = True
 USE_TZ = True
 
 LOCALE_PATHS = [
-    BASE_DIR / 'locale'
+    os.path.join(BASE_DIR, 'locale')
 ]
 USE_RTL = True
 # Static files (CSS, JavaScript, Images)
