2cc61dc3e5d0a906c4c6a0a148a615c1c2cfbbfc
alabri0803 <rashid92759346@gmail.com>
2025-04-13 13:48:15 +0400

    n

--- realestate/settings.py
+++ realestate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^*je6erypv%pbadqr-836(gv=0-b^b!jxt98o(q!yye1#_w5dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
